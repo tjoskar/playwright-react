790e5c0384a4afe7cb09c85c7ca4b84cdb7c896a
tjoskar <kontakta@oskarkarlsson.nu>
2021-10-16 17:18:17 +0200

    🦧

--- example/src/components/MyButton.spec.ts
+++ example/src/components/MyButton.spec.ts
@@ -2,16 +2,12 @@ import { expect, test } from '@playwright/test';
 import { createElement } from 'react';
 import { setup } from "playwright-react";
 
-// Set up the components you want to test. You can add as many as you want.
 const mount = setup(__dirname, {
   MyButton: () => import('./MyButton').then(c => c.MyButton),
 });
 
 test('Click on MyButton', async ({ page }) => {
-  await mount(page, ({ MyButton }) => {
-    // You will get typescript intellisense here
-    return createElement(MyButton);
-  });
+  await mount(page, ({ MyButton }) => createElement(MyButton));
 
   await page.click('text=Click on me');
 
@@ -19,10 +15,7 @@ test('Click on MyButton', async ({ page }) => {
 });
 
 test('MyButton is a button', async ({ page }) => {
-  await mount(page, ({ MyButton }) => {
-    // You will get typescript intellisense here
-    return createElement(MyButton);
-  });
+  await mount(page, ({ MyButton }) => createElement(MyButton));
 
   await expect(page.locator('button')).toBeVisible();
 });
